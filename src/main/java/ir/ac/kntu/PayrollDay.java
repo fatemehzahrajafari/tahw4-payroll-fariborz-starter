c6413e43060ea310e7f98c9f31facb364491a34b
fatemehzahrajafari <47676074+fatemehzahrajafari@users.noreply.github.com>
2019-04-19 23:55:46 +0430

    Update PayrollDay.java

--- src/main/java/ir/ac/kntu/PayrollDay.java
+++ src/main/java/ir/ac/kntu/PayrollDay.java
@@ -1,9 +1,54 @@
     enum PayrollDay {
-    // enum members goes here;
+	SUNDAY, MONDAY, TUESDAY, WEDNESDAY, 
+    THURSDAY, FRIDAY, SATURDAY; 
+
+	public pay(PayrollDay payrollday){
+		this.payrollday=payrollday;
+	}
 
     private static final int MINS_PER_SHIFT = 8 * 60;
 
     int pay(int minutesWorked, int payRate) {
-        //TODO
+	Scanner scanner=new scanner(System.in);
+	
+	int minutesWorked=scanner.nextInt();
+	int payRate=scanner.nextInt();
+	int overtime=scanner.nextInt();
+        switch(PayrollDay)
+	{
+	case SATURDAY:
+	PayRate=MINS-PER-SHIFT*minutesWorked;
+	System.out.println(payRate);
+	break;
+	case SUNDAY:
+	
+	PayRate=MINS-PER-SHIFT*minutesWorked;
+	System.out.println(payRate);
+	
+	break;
+	case MONDAY:
+	
+	PayRate=MINS-PER-SHIFT*minutesWorked;
+	System.out.println(payRate);
+	break;
+	case TUESDAY:
+	
+	PayRate=MINS-PER-SHIFT*minutesWorked;
+	System.out.println(payRate);
+	break;
+	case WEDNESDAY:
+	
+	PayRate=MINS-PER-SHIFT*minutesWorked;
+	System.out.println(payRate);
+	break;
+	case THURSDAY:
+	payRate=(1.5*overtime+MINS-PER-SHIFT)*minutesWorked;
+	System.out.println(payRate);
+	break;
+	case FRIDAY:
+	payRate=(1.5*overtime+MINS-PER-SHIFT)*minutesWorked;
+	System.out.println(payRate);
+	break;
+
     }
 } 
